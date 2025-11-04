03e5cf7521757888bd77c160201c51b736024041
Melisa <melisa29mona@gmail.com>
2025-11-04 13:29:26 -0500

    adelanto Card

--- 02-sintaxis-react/src/components/ExtensionCard.jsx
+++ 02-sintaxis-react/src/components/ExtensionCard.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import "../stylesheets/ExtensionCard.css"
 
 export const ExtensionCard = () => {
   return (
-    <div>ExtensionCard</div>
+    <>
+    <div className="camara" >
+    <img className="camarita" src="./public/camara.png" alt="" />
+    <h1 className="parrofo" >Quickly inspect page layoust and visualize element boundaries</h1>
+
+
+    </div>
+    </>
+    
   )
 }
